5d31f5e8bcea4fa5fc79456e6651d0a1e25208be
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-09-09 15:33:00 -0500

    reorganize code and update styles

--- content.js
+++ content.js
@@ -1,28 +1,38 @@
-console.log("extension running...");
+function getSelector(origin, name) {
+  const iconUrl = `images/icons/priorities/${name}.svg`;
+  return `[src="/${iconUrl}"], [src="${origin}/${iconUrl}"]`
+}
+
+function applyStyles(node, styles) {
+  for (key in styles) {
+    node.style[key] = styles[key];
+  }
+}
 
 function inject() {
-  const getIconUrl = name => `/images/icons/priorities/${name}.svg`;
-
-  const rotations = {
-    high: 45,
-    low: -45
+  const transformations = {
+    high: {
+      transform: "rotate(45deg)"
+    },
+    medium: {
+      transform: "rotate(90deg)",
+      opacity: 0.5
+    },
+    low: {
+      transform: "rotate(-45deg)",
+      opacity: 0.5
+    },
+    trivial: {
+      opacity: 0.5
+    }
   };
 
-  // Rotate the types that need rotation
-  Object.entries(rotations).forEach(([key, value]) => {
-    const arrows = document.querySelectorAll(`[src="${getIconUrl(key)}"]`);
-    arrows.forEach(arrow => {
-      arrow.style.transform = `rotate(${value}deg)`;
-    });
-  });
-
-  // Hide medium arrows
-  const mediumArrows = document.querySelectorAll(
-    `[src="${getIconUrl("medium")}"]`
-  );
-  mediumArrows.forEach(arrow => {
-    arrow.style.transform = 'rotate(90deg)'
-    arrow.style.opacity = 0.5;
+  // Style the types that need transformations
+  Object.entries(transformations).forEach(([key, styles]) => {
+    const origin = window.location.origin;
+    const selector = getSelector(origin, key);
+    const arrows = document.querySelectorAll(selector);
+    arrows.forEach(arrow => applyStyles(arrow, styles));
   });
 }
 
