562f6269280cc4a18283213757194b11841e0d3d
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-09-09 16:47:20 -0500

    another minor fix

--- content.js
+++ content.js
@@ -27,8 +27,8 @@ function applyStyles(node, styles) {
 }
 
 function inject() {
+  const origin = window.location.origin;
   Object.entries(transformations).forEach(([key, styles]) => {
-    const origin = window.location.origin;
     const selector = getSelector(origin, key);
     const arrows = document.querySelectorAll(selector);
     arrows.forEach(arrow => applyStyles(arrow, styles));
